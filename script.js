4859d13e9ce05ccc516bb0b8d3762ec13a6f64df
Ivo Gulin <69385144+IvoGulin@users.noreply.github.com>
2021-06-09 12:16:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function fun()
 	var glavnica = document.getElementById("unos1").value;
 	var postotak = document.getElementById("unos2").value;
 	var brojrata = document.getElementById("unos3").value;
+	var placa = document.getElementById("unos4").value;
 	
 	var uglavnica = parseFloat(glavnica) + parseFloat(glavnica * postotak/100);
-	//alert(uglavnica);
-	
+	//alert (uglavnica);
 	var rata = parseFloat(uglavnica/brojrata);
-	//alert(rata);
-	document.getElementById("rez").innerHTML = rata;
+	var uplaca = placa - rata;
+	
+	document.getElementById("rez1").innerHTML = rata;
+	document.getElementById("rez2").innerHTML = uplaca;
 }
